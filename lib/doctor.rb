e997db036cf515c668c087c424c2cb72f06decc9
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-17 21:22:54 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Doctor
+  @@all=[]
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
+  end
+
+  def patients
+
+   Appointment.all.map do |appointment|
+     appointment.patient
+
+   end
+
+ end
+
+end
